e6a3fae41bc374d88c90b7ccab174d66e527b1e7
iL53n <ilsegribko@gmail.com>
2021-10-16 14:34:56 +0300

    fix(config): icon set

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -4,9 +4,15 @@
       q-btn(flat round dense icon="menu" class="q-mr-sm")
       q-avatar
         img(src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg")
-      q-toolbar-title
-        | ChildSchedule
+      //q-toolbar-title(class="title") ChildSchedule
+      q-toolbar-title ChildSchedule
       q-btn(flat round dense icon="whatshot")
 </template>
 
 <script></script>
+
+<style scoped>
+  .title {
+    color: yellow;
+  }
+</style>
--- app/javascript/packs/quasar_params.js
+++ app/javascript/packs/quasar_params.js
@@ -1,6 +1,7 @@
 // Quasar framework
-import iconSet from 'quasar/icon-set/fontawesome-v5.js'
-
+// import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import iconSet from 'quasar/icon-set/material-icons'
+import '@quasar/extras/material-icons/material-icons.css'
 import {
     QLayout,
     QHeader,
