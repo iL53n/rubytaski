4f637a253cf02a5f67515775d41afc7dadc2f394
iL53n <ilsegribko@gmail.com>
2021-10-16 10:03:34 +0300

    config(app): add pug

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,12 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const babelLoaderQuasar = require('./loaders/babel-loader-quasar')
 // const stylus = require('./loaders/stylus')
+const pugConfig = require('./loaders/pug')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('js', babelLoaderQuasar)
 // environment.loaders.append('stylus', stylus)
+environment.config.merge(pugConfig)
 environment.loaders.prepend('vue', vue)
 module.exports = environment
