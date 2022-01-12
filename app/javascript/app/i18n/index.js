be9fffbfe661a93d3732b1fc6c3d915022aad295
iL53n <ilsegribko@gmail.com>
2022-01-12 17:11:36 +0300

    add locales folder

--- app/javascript/app/i18n/index.js
+++ app/javascript/app/i18n/index.js
@@ -1,52 +1,17 @@
- import Vue     from 'vue'
- import VueI18n from 'vue-i18n'
+import Vue     from 'vue'
+import VueI18n from 'vue-i18n'
 
- Vue.use(VueI18n)
+import { ru } from './locales/ru'
+import { en } from './locales/en'
 
- const messages = {
-   en: {
-     stat: {
-      //  example: '{msg} world',
-      today: 'Today',
-      week: 'Week',
-      goal: 'Goal'
-     },
-     heatmap: {
-      title: 'Heatmap',
-      unit: 'stars',
-      locale: {
-        months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        on: 'on',
-        less: 'Less',
-        more: 'More'
-      }
-    }
-   },
-   ru: {
-    stat: {
-      today: 'Сегодня',
-      week: 'За неделю',
-      goal: 'Цель'
-    },
-    heatmap: {
-      title: 'Карта выполнения',
-      unit: 'звезд',
-      locale: {
-        months: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-        days: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        on: '-',
-        less: 'Мнш',
-        more: 'Блш'
-      }
-    }
-  }
- }
+Vue.use(VueI18n)
 
- const i18n = new VueI18n({
-   locale: 'en',
-   fallbackLocale: 'en',
-   messages: messages
- })
+const messages = { ru, en }
 
- export default i18n
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: messages
+})
+
+export default i18n
