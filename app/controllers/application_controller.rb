13e9a33bdb51ad8845c411f58f2273738ca9a01d
iL53n <ilsegribko@gmail.com>
2022-01-14 09:21:53 +0300

    add set locale to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
 
   # def after_sign_in_path_for(resource)
   #   if resource.admin?
@@ -13,4 +14,13 @@ class ApplicationController < ActionController::Base
   def index; end
 
   def locale; end
+
+  private
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.locale = locale
+  end
 end
