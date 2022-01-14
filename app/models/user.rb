e75916770f0dde25c9c85765731b00cc611e7c9b
iL53n <ilsegribko@gmail.com>
2022-01-14 18:26:04 +0300

    add set nick_name before action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  EMAIL = /(?<login>\S+)\@(?<domain>\S+)/.freeze
+  before_save :set_nick_name
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +10,15 @@ class User < ApplicationRecord
 
   has_many :tasks
   has_many :stars
+
+  private
+
+  def set_nick_name
+    self.nick_name ||= login
+    # logger.debug "* Set nick_name: '#{self.nick_name}'"
+  end
+
+  def login
+    email.match(EMAIL)[:login]
+  end
 end
