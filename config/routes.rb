f52455e99057b612288bee5d71847dbba6bc630e
iL53n <ilsegribko@gmail.com>
2022-02-10 11:57:48 +0300

    add constraints (format: :json) to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,26 @@ Rails.application.routes.draw do
     get :me, on: :collection
   end
 
-  resources :tasks, only: %i[index create show update destroy] do
-    patch :update_order, to: 'tasks#update_order', on: :collection
+  constraints format: :json do
+    resources :tasks, only: %i[index create show update destroy] do
+      patch :update_order, to: 'tasks#update_order', on: :collection
+    end
+
+    resources :goals, only: %i[index create show update destroy] do
+      patch :set_state, to: 'goals#set_state', on: :member
+    end
+
+    resources :stars, only: %i[create destroy]
+
+    # TODO: add set locale in user profile
+    get :locale, to: 'application#locale'
+
+    # Stat_data
+    # TODO: add scope 'stat'
+    get '/heatmap_stat', to: 'stars#heatmap_stat'
+    get '/stars_stat',   to: 'stars#stars_stat'
+    get '/current_stat', to: 'stars#current_stat'
   end
-
-  resources :goals, only: %i[index create show update destroy] do
-    patch :set_state, to: 'goals#set_state', on: :member
-  end
-
-  resources :stars, only: %i[create destroy]
-
-  # TODO: add set locale in user profile
-  get :locale, to: 'application#locale'
-
-  # Stat_data
-  # TODO: add scope 'stat'
-  get '/heatmap_stat', to: 'stars#heatmap_stat'
-  get '/stars_stat',   to: 'stars#stars_stat'
-  get '/current_stat', to: 'stars#current_stat'
 
   get '/*path', to: 'application#index', format: false
 end
