2695c672769d60bad3663b682648608aab7ef08a
iL53n <ilsegribko@gmail.com>
2022-03-13 18:17:26 +0300

    add page statistics template

--- app/javascript/app/components/Statistics.vue
+++ app/javascript/app/components/Statistics.vue
@@ -1,6 +1,59 @@
 <template lang="pug">
-    div(class="col text-h3 text-blue-grey-14") {{ $t('stat.title') }}
-
+  div
+    .text-h3.text-blue-grey-14 {{ $t('stat.title') }} DEMO
+    .row.justify-around
+      q-card.col.q-ma-md
+        q-card-section(horizontal)
+          q-card-section.col(align="left")
+            .text-h3.text-blue-grey-14 20
+            .text-caption.text-blue-grey-8 Tasks
+          q-card-section.flex.flex-center(align="right")
+            q-btn.text-blue-4.bg-blue-1(unelevated round size="lg" icon="format_list_bulleted")
+      q-card.col.q-ma-md
+        q-card-section(horizontal)
+          q-card-section.col(align="left")
+            .text-h3.text-blue-grey-14 375
+            .text-caption.text-blue-grey-8 Completed
+          q-card-section.flex.flex-center(align="right")
+            q-btn.text-green-4.bg-green-1(unelevated round size="lg" icon="done_all")
+      q-card.col.q-ma-md
+        q-card-section(horizontal)
+          q-card-section.col(align="left")
+            .text-h3.text-blue-grey-14 75%
+            .text-caption.text-blue-grey-8 Efficiency
+          q-card-section.flex.flex-center(align="right")
+            q-btn.text-red-4.bg-red-1(unelevated round size="lg" icon="speed")
+      q-card.col.q-ma-md
+        q-card-section(horizontal)
+          q-card-section.col(align="left")
+            .text-h3.text-blue-grey-14 88
+            .text-caption.text-blue-grey-8 Days
+          q-card-section.flex.flex-center(align="right")
+            q-btn.text-teal-4.bg-teal-1(unelevated round size="lg" icon="date_range")
+    .row.justify-around
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="bar" :options="options" :series="series")
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="line" :options="options" :series="series")
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="area" :options="options" :series="series")
+    .row.justify-around
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="heatmap" :options="options" :series="series")
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="scatter" :options="options" :series="series")
+    .row.justify-around
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="radar" :options="options" :series="series")
+      q-card.col.q-ma-md
+        | DEMO
+        apexchart(height="200" width="500" type="area" :options="options" :series="series")
 </template>
 
 <script>
@@ -8,8 +61,19 @@
   export default {
     data () {
       return {
-
-      } 
+        options: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'demo_series',
+          data: [30, 40, 15, 60, 80, 10, 90, 70]
+        }]
+      }
     },
     created() {
 
