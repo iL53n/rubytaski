e390e8cef0af0f704e937a976c8d975dce03977f
iL53n <ilsegribko@gmail.com>
2022-03-15 14:14:18 +0300

    fix general stat view

--- app/javascript/app/components/GeneralStat.vue
+++ app/javascript/app/components/GeneralStat.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   div(class="row no-wrap")
     div(class="col text-uppercase" align="left")
-      .text-h1.text-white {{ stats.all }}
+      .text-h1.text-white {{ stars_stat.all }}
       .text-subtitle1.text-grey-1 {{ $t('stat.all_stars') }}
       br
-      .text-h2.text-white {{ stats.current_year }}
+      .text-h2.text-white {{ stars_stat.current_year }}
       .text-subtitle2.text-grey-1 {{ $t('stat.this_year') }}
       br
-      .text-h2.text-white {{ stats.current_month }}
+      .text-h2.text-white {{ stars_stat.current_month }}
       .text-subtitle2.text-grey-1 {{ $t('stat.this_month') }}
       br
-      .text-h4.text-white {{ stats.current_week }}
+      .text-h4.text-white {{ stars_stat.current_week }}
       .text-subtitle2.text-grey-1 {{ $t('stat.this_week') }}
       br
-      .text-h5.text-white {{ stats.current_day }}
+      .text-h5.text-white {{ stars_stat.current_day }}
       .text-subtitle2.text-grey-1 {{ $t('stat.today') }}
     div(class="col-auto" align="bottom")
       q-btn(color="grey-1" round flat icon="more_vert")
@@ -32,7 +32,7 @@
       }
     },
     computed: {
-      stats() {
+      stars_stat() {
         return this.$store.state.statistics.stars
       }
     },
