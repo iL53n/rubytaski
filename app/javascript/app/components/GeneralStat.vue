5673f07fbea9542b1398cc524ce1ea048e29683d
iL53n <ilsegribko@gmail.com>
2022-08-24 19:57:40 +0400

    add astroman image to general stat

--- app/javascript/app/components/GeneralStat.vue
+++ app/javascript/app/components/GeneralStat.vue
@@ -1,28 +1,28 @@
 <template lang="pug">
-  div(class="row no-wrap")
-    div(class="col text-uppercase" align="left")
-      .text-h2.text-white {{ stars_stat.all }}
-      .text-caption.text-grey-1 {{ $t('stat.all_stars') }}
-      br
-      .text-h3.text-white {{ stars_stat.current_year }}
-      .text-caption.text-grey-1 {{ $t('stat.this_year') }}
-      br
-      .text-h4.text-white {{ stars_stat.current_month }}
-      .text-caption.text-grey-1 {{ $t('stat.this_month') }}
-      br
-      .text-h5.text-white {{ stars_stat.current_week }}
-      .text-caption.text-grey-1 {{ $t('stat.this_week') }}
-      br
-      .text-h6.text-white {{ stars_stat.current_day }}
-      .text-caption.text-grey-1 {{ $t('stat.today') }}
-    div(class="col-auto" align="bottom")
-      q-btn(color="grey-1" round flat icon="more_vert")
-        q-menu(cover auto-close)
-          q-list
-            q-item(clickable to="/my_statistics" class="text-primary")
-              q-item-section {{ $t('stat.show_all') }}
-          //- q-item(clickable)
-          //-   q-item-section Send Feedback
+  div
+    div(class="row no-wrap")
+      div(class="col text-uppercase" align="left")
+        .text-h2.text-white {{ stars_stat.all }}
+        .text-caption.text-grey-1 {{ $t('stat.all_stars') }}
+        br
+        .text-h3.text-white {{ stars_stat.current_year }}
+        .text-caption.text-grey-1 {{ $t('stat.this_year') }}
+        br
+        .text-h4.text-white {{ stars_stat.current_month }}
+        .text-caption.text-grey-1 {{ $t('stat.this_month') }}
+        br
+        .text-h5.text-white {{ stars_stat.current_week }}
+        .text-caption.text-grey-1 {{ $t('stat.this_week') }}
+        br
+        .text-h6.text-white {{ stars_stat.current_day }}
+        .text-caption.text-grey-1 {{ $t('stat.today') }}
+      div(class="col-auto" align="bottom")
+        q-btn(color="grey-1" round flat icon="more_vert")
+          q-menu(cover auto-close)
+            q-list
+              q-item(clickable to="/my_statistics" class="text-primary")
+                q-item-section {{ $t('stat.show_all') }}
+    q-img(:src="require('images/astroman.png')" style="bottom: 40px; left: 40px")
 </template>
 
 <script>
