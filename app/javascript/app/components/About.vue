2b5732a6d5d3460997f312ecb7b47e4fe7553fe3
iL53n <ilsegribko@gmail.com>
2022-12-01 22:03:35 +0400

    add some fixes to about page

--- app/javascript/app/components/About.vue
+++ app/javascript/app/components/About.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   .q-pa-xl.row.items-start.justify-around.q-gutter-xl
     q-card.my-card
-      q-img(src="https://eufordigital.eu/wp-content/uploads/2019/08/georgia.jpg")
-        .absolute-bottom.text-h6 RubyTaski
+      q-img(src="https://coolhdwall.com/storage/202101/astronaut-floating-space-2k-wallpaper-3840x1600.jpg")
+        .absolute-bottom.text-right.text-h3 rubytaski
       q-card-section
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        | eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        | eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        | eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        | eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        | eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        div(class="text-h5 text-left") We do what we love and
+        div(class="text-h5 text-left") we love what we do
+        div(class="text-subtitle1 text-left text-blue-grey-14")
+          | We love to create tools for improving Kids' and Teens' life,
+          | because in this way we improve the future, we change it.
+        br
+        br
+        div(class="text-body2 text-grey")
+          | To learn more about the team, or have any questions,
+          | you're more than welcome to contact us at <a href = "mailto: hello@rubytaski.com">hello@rubytaski.com</a>.
       q-card-section.text-caption.text-grey © {{currentYear}} RubyTaski team
 </template>
 
@@ -43,7 +43,7 @@ export default {
 <style  scoped>
 .my-card {
   width: 100%;
-  max-width: 500px
+  max-width: 600px
   }
 </style>
 
